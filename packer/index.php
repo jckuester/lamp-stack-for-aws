bf44b7e9cc1c1603eac6196dae93313c44513e14
Jan-Christoph Kuester <jckuester@gmail.com>
2017-03-27 21:09:41 +0200

    Add index.php using mysql

--- packer/index.php
+++ packer/index.php
@@ -1,3 +1,70 @@
+<html>
+<head>
+<title>LAMP Stack Reloaded: An AWS example</title>
+</head>
+
+<body>
+<h1>LAMP Stack Reloaded: An AWS example</h1>
+
 <?php
-phpinfo();
+$servername = "tf-001473eca1515e7fe20d6e2330.cbca6q4otsut.us-west-2.rds.amazonaws.com";
+$username = "lamp";
+$password = "lamp1234";
+$db = "lamp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+print "Connected successfully to " . $servername . "!<br>";
+
+// sql to create table
+$sql = "CREATE TABLE Lamp2 (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+sometext VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table  created successfully!";
+} else {
+    //echo "Error creating table: " . $conn->error . "<br>";
+}
+
+$value = "Bla";
+$sql = "INSERT INTO Lamp2 (sometext) VALUES ('" . $value . "')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record '" . $value . "' created successfully! <br><br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "SELECT id, sometext FROM Lamp2";
+$result = $conn->query($sql);
+
+print "<table border=1>
+  <tr>
+    <th>ID</th>
+    <th>Some text</th>
+  </tr>";
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["sometext"] . "</td></tr>";
+    }
+} else {
+    echo "0 results";
+}
+
+print "</table>";
+
+$conn->close();
 ?>
+</body>
+
+</html>
