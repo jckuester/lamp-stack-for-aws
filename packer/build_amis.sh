ac37261a40bd94c9383986303f60ecbe1e46289d
Jan-Christoph Kuester <jckuester@gmail.com>
2017-03-28 16:33:14 +0200

    Improve packer wrapper script

--- packer/build_amis.sh
+++ packer/build_amis.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 function main {
-    if [ $# != 1 ]; then
-        echo "Usage: $0 [build-version]"
+    if [ $# != 2 ]; then
+        echo "Usage: $0 [profile] [build-version]"
         exit 1
     fi
 
+    local profile=$1; shift
     local version=$1; shift
-    local region=$(aws --profile lamp configure get region || true)
-    
+    local region=$(aws --profile $profile configure get region || true)
+
     if [ -z "$region" ]; then
         echo "Error: region for profile 'lamp' not set"
         exit 1
     fi
 
-    AWS_PROFILE=lamp packer build \
+    AWS_PROFILE="$profile" packer build \
         -var "region=$region" \
         -var "build_version=$version" \
         lamp.json
