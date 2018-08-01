a7b578f176b424d296910f6be49b77fd93946d12
Jan-Christoph Kuester <jckuester@gmail.com>
2018-08-01 16:15:30 +0200

    Make index.php pretty

--- terraform/modules/webserver/index.php
+++ terraform/modules/webserver/index.php
@@ -3,11 +3,11 @@
 <title>Terraform example project: LAMP Stack in AWS</title>
 </head>
 
-<body>
-<div style="position: absolute; left: 50%; margin-left: -400px; width:800px; background: white; border: 2px solid black">
-<center>
-<h2>Terraform example project: LAMP Stack in AWS</h2>
-
+<body style="background-color: gray">
+<div id="container">
+<div style="margin:0 auto; text-align:center; width:1200px; height: 650px; background: white; border: 2px solid black">
+<h1 style="font-size: 4em">Terraform example project</h1>
+<h2 style="font-size: 2em">LAMP Stack in AWS</h2>
 <?php
 
 /*
@@ -21,28 +21,23 @@ $username = "lamp";
 $password = "lamp1234";
 $db = "lamp";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $db);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-print 'Connected successfully to DB.';
 
-// sql to create table
 $sql = "CREATE TABLE lamp (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     sometext VARCHAR(50),
     reg_date TIMESTAMP
 )";
-
 if ($conn->query($sql) === TRUE) {
-    echo "Table  created successfully!";
+    echo "Table created successfully!";
 }
 
 print '<form action="index.php" method="post">
-Some text: <input type="text" name="sometext"><br>
-<input type="submit">
+<input type="text" name="sometext" placeholder="Add some text ...">
+<input class="button" type="submit">
 </form>';
 
 if(isset($_POST['sometext'])) {
@@ -61,15 +56,14 @@ if(isset($_POST['sometext'])) {
 $sql = "SELECT id, sometext FROM lamp";
 $result = $conn->query($sql);
 
-print "<table border=1>
+print "<table>
   <tr>
-    <th>ID</th>
     <th>Some text</th>
   </tr>";
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["sometext"] . "</td></tr>";
+        echo "<tr><td>" . $row["sometext"] . "</td></tr>";
     }
 } else {
     echo "0 results";
@@ -81,7 +75,56 @@ $conn->close();
 ?>
 
 <br><br>
-</center>
+</div>
 </div>
 </body>
+
+<style>
+table {
+    border-collapse: collapse;
+    margin: 0 auto;
+}
+
+table, th, td {
+    border: 1px solid black;
+    padding: 10px;
+}
+
+th {
+    background-color: lightgray; /* Green */
+}
+
+.button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 12px 30px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 4px;
+}
+
+input[type=text], select {
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+#container {
+    position: absolute;
+    top: 50%;
+    margin-top: -325px;
+    /* half of #content height*/
+    left: 0;
+    width: 100%;
+}
+
+</style>
 </html>
